508e46be02cb20b048bd8d1fdc1bc557cd26a308
Haoyu Weng <wenghy02@gmail.com>
2025-02-26 21:18:15 -0800

    add comments

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/python/PythonScanBuilder.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/python/PythonScanBuilder.scala
@@ -33,9 +33,14 @@ class PythonScanBuilder(
 
   override def build(): Scan = new PythonScan(ds, shortName, outputSchema, options)
 
+  // Optionally called by DSv2 once to push down filters before the scan is built.
   override def pushFilters(filters: Array[Filter]): Array[Filter] = {
     val dataSource = ds.getOrCreateDataSourceInPython(shortName, options, Some(outputSchema))
     val result = ds.source.pushdownFiltersInPython(dataSource, outputSchema, filters)
+
+    // The Data Source instance state changes after pushdown to remember the reader instance
+    // created and the filters pushed down. So pushdownFiltersInPython returns a new pickled
+    // Data Source instance. We need to use that new instance for further operations.
     ds.setDataSourceInPython(dataSource.copy(dataSource = result.dataSource))
 
     // Partition the filters into supported and unsupported ones.
